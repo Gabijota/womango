d098fd7582bda97ac1b00ac4a5f49c6bfb3c6187
Gabija <gjamontaite@gmail.com>
2018-10-10 08:54:56 +0300

    keiciau i php faila

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 </head>
 
 <body>
-
+<header>
     <div class="konteinerio-pirmas">
         <div class="container">
             <div class="row">
@@ -57,40 +57,4 @@
             </div>
         </div>
     </div>
-
-    <div class="konteinerio-antras">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="intro">
-                        <h3><b>Empowering Talents To Discover Tech</b></h3>
-                        <p>"Woman Go Tech"- pirmoji mentorystės programa Lietuvoje skirta studentėms ir profesinį kelią
-                            pradėjusioms moterims kurias domina karjera technologijų sektoriuje.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="konteinerio-trecias">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="didelispic">
-                    <img src="pic/tech.png" alt="tech">
-                </div>
-            </div>
-        </div>
-    </div>
-    </div>
-
-
-
-
-
-
-
-
-</body>
-
-</html>
+</header>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php include 'header.php';  ?>
+      
+
+
+<div class="konteinerio-antras">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="intro">
+                        <h3><b>Empowering Talents To Discover Tech</b></h3>
+                        <p>"Woman Go Tech"- pirmoji mentorystės programa Lietuvoje skirta studentėms ir profesinį kelią
+                            pradėjusioms moterims kurias domina karjera technologijų sektoriuje.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+
+        <div class="konteinerio-trecias">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="didelispic">
+                        <img src="pic/tech.png" alt="tech">
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+
+
+
+        <?php include 'footer.php';?>
+
+
+</body>
+</html>        
+        
